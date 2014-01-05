6d9f4d239dfe85fd79551ab6ef881ec696553716
asuka4j <asuka4j@gmail.com>
2014-01-05 11:14:41 +0800

    add secert

--- Gemfile
+++ Gemfile
@@ -1,10 +1,20 @@
 source 'https://rubygems.org'
+ruby '2.0.0'
+
+group :development do
+	# Use sqlite3 as the database for Active Record
+	gem 'sqlite3'
+	gem 'rspec-rails', '2.13.1'
+end
+
+group :test do
+	gem 'selenium-webdriver', '2.35.1'
+	gem 'capybara', '2.1.0'
+end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,33 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '266aa6cd62a2c16dd6fb45cc885a5772b5490149896931c40305e871427e8f67cae1a03578fd222e9f89676f702b78941191609309a40423ea0250c2c4f10346'
+# SampleApp::Application.config.secret_key_base = '266aa6cd62a2c16dd6fb45cc885a5772b5490149896931c40305e871427e8f67cae1a03578fd222e9f89676f702b78941191609309a40423ea0250c2c4f10346'
+
+
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
